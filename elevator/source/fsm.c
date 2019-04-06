108e573f6a24a5bf06ee9c3935e529616752c81f
olavpe <olavpe@gmail.com>
2019-04-06 12:27:34 +0200

    adding the files for fsm.c and fsm.h

---
+++ elevator/source/fsm.c
@@ -0,0 +1,9 @@
+#include "elevator_hardware.h"
+#include "orders.h"
+//#include "timer.h"
+
+#include <stdio.h>
+
+void fsm(){
+
+}
---
+++ elevator/source/fsm.h
@@ -0,0 +1,14 @@
+#pragma once
+
+typedef enum position_t {
+    FLOOR_0,
+    FLOOR_1,
+    FLOOR_2,
+    FLOOR_3,
+    BETWEEN_0_AND_1,    
+    BETWEEN_1_AND_2,
+    BETWEEN_2_AND_3
+}
+
+// functions
+void fsm();
