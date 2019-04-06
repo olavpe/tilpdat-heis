971289e4ec8687b5e5f7fd3a6ae23f0b39c3d22c
olavpe <olavpe@gmail.com>
2019-04-06 12:26:45 +0200

    adding lights to register_order_press

--- elevator/source/main.c
+++ elevator/source/main.c
@@ -1,25 +1,20 @@
-//#include "elev.h"
-#include "elevator_hardware.h"
 #include <stdio.h>
 
+//#include "elev.h"
+#include "elevator_hardware.h"
 
-int main() {
+int8_t main() {
     // Initialize hardware
     elev_init();
-//    if (!elev_init()) {
-//        printf("Unable to initialize elevator hardware!\n");
-//        return 1;
-//    }
-
-//    elev_set_motor_direction(DIRN_UP);
 
     while (1) {
+        
         char floor_signal = elev_get_floor_sensor_signal();
-        printf("floor_signal: %d\n", (char)floor_signal);
+        print8_tf("floor_signal: %d\n", (char)floor_signal);
         
         elev_set_floor_indicator(floor_signal);
         elev_button_type_t buttons = elev_get_button_signal(DIRN_DOWN, 3);
-        printf("buttonTest: %d\n", (int)buttons);
+        print8_tf("buttonTest: %d\n", (int8_t)buttons);
         // Change direction when we reach top/bottom floor
 //        if (elev_get_floor_sensor_signal() == N_FLOORS - 1) {
 //            elev_set_motor_direction(DIRN_DOWN);
@@ -33,36 +28,17 @@ int main() {
 //            break;
 //        }
     }
-
     return 0;
 }
 
-//int main() {
-//    // Initialize hardware
-//    if (!elev_init()) {
-//        printf("Unable to initialize elevator hardware!\n");
-//        return 1;
-//    }
-//
-//    printf("Press STOP button to stop elevator and exit program.\n");
-//
-//    elev_set_motor_direction(DIRN_UP);
-//
-//    while (1) {
-//        // Change direction when we reach top/bottom floor
-//        if (elev_get_floor_sensor_signal() == N_FLOORS - 1) {
-//            elev_set_motor_direction(DIRN_DOWN);
-//        } else if (elev_get_floor_sensor_signal() == 0) {
-//            elev_set_motor_direction(DIRN_UP);
-//        }
-//
-//        // Stop elevator and exit program if the stop button is pressed
-//        if (elev_get_stop_signal()) {
-//            elev_set_motor_direction(DIRN_STOP);
-//            break;
-//        }
-//    }
-//
-//    return 0;
-//}
-//
+static void m_register_order_press(){
+    for (int8_t button = 0; button < N_BUTTONS; button++) {
+        for (int8_t floor = 0; floor < N_FLOORS; floor++) {
+            int8_t button_signal = elev_get_button_signal(button, floor);
+            if (button_signal == 1) {
+                queue_set_order(button, floor);
+                elev_set_button_lamp(button, floor, 1);
+            }
+        }
+    }
+}
